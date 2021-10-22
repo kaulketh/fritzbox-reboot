7b55d425ff07cbd1d56efef67aa3b59f32b151be
Thomas <kaulketh@gmail.com>
2021-10-23 00:50:52 +0200

    update

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
+from python.fritzbox_reboot import execute_curl, NO_USER
+from python.secret import   BOX_IP, BOX_USER, BOX_USER_PW, REP_IP_1, \
+    REP_PW, REP_IP_2
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#execute_curl(BOX_IP, BOX_USER, BOX_USER_PW)
+#execute_curl(REP_IP_1, NO_USER, REP_PW)
+execute_curl(REP_IP_2, NO_USER, REP_PW)
